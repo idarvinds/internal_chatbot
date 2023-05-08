1bc21f5ec92bb170cc6180a9b0a2b2f2df9708b1
Arvind Sharma <arvind@sortfolio.com>
2023-05-09 04:52:27 +0530

    Added password protection

--- app.py
+++ app.py
@@ -10,68 +10,104 @@ from langchain import OpenAI
 doc_path = './storage/'
 index_file = 'index.json'
 
-if 'response' not in st.session_state:
-    st.session_state.response = ''
+def check_password():
+    """Returns `True` if the user had the correct password."""
 
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+    
 def send_click():
     query_engine = index.as_query_engine()
     st.session_state.response  = query_engine.query(st.session_state.prompt)
 
-index = None
-st.title("Moxie Properties Chatbot")
+if check_password():
+    if 'response' not in st.session_state:
+        st.session_state.response = ''
 
-sidebar_placeholder = st.sidebar.container()
-uploaded_file = st.file_uploader("Choose a file")
+    index = None
+    st.title("Moxie Properties Chatbot")
 
-if uploaded_file is not None:
+    sidebar_placeholder = st.sidebar.container()
+    uploaded_file = st.file_uploader("Choose a file")
 
-    doc_files = os.listdir(doc_path)
-    for doc_file in doc_files:
-        os.remove(doc_path + doc_file)
+    if uploaded_file is not None:
 
-    bytes_data = uploaded_file.read()
-    with open(f"{doc_path}{uploaded_file.name}", 'wb') as f: 
-        f.write(bytes_data)
+        doc_files = os.listdir(doc_path)
+        for doc_file in doc_files:
+            os.remove(doc_path + doc_file)
 
-    SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+        bytes_data = uploaded_file.read()
+        with open(f"{doc_path}{uploaded_file.name}", 'wb') as f: 
+            f.write(bytes_data)
 
-    loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
-    documents = loader.load_data()
-    sidebar_placeholder.header('Current Processing Document:')
-    sidebar_placeholder.subheader(uploaded_file.name)
-    sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+        SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
 
-    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
+        loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
+        documents = loader.load_data()
+        sidebar_placeholder.header('Current Processing Document:')
+        sidebar_placeholder.subheader(uploaded_file.name)
+        sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
 
-    max_input_size = 4096
-    num_output = 256
-    max_chunk_overlap = 20
-    prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
+        llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
 
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
+        max_input_size = 4096
+        num_output = 256
+        max_chunk_overlap = 20
+        prompt_helper = PromptHelper(max_input_size, num_output, max_chunk_overlap)
 
-    index = GPTVectorStoreIndex.from_documents(
-        documents, service_context=service_context
-    )
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=prompt_helper)
 
-    index.storage_context.persist()
+        index = GPTVectorStoreIndex.from_documents(
+            documents, service_context=service_context
+        )
 
-elif os.path.exists(index_file):
-    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+        index.storage_context.persist()
 
-    index = load_index_from_storage(storage_context)
+    elif os.path.exists(index_file):
+        storage_context = StorageContext.from_defaults(persist_dir="./storage")
 
-    SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
-    loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
-    documents = loader.load_data()
-    doc_filename = os.listdir(doc_path)[0]
-    sidebar_placeholder.header('Current Processing Document:')
-    sidebar_placeholder.subheader(doc_filename)
-    sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+        index = load_index_from_storage(storage_context)
 
-if index != None:
-    st.text_input("Ask something: ", key='prompt')
-    st.button("Send", on_click=send_click)
-    if st.session_state.response:
-        st.subheader("Response: ")
-        st.success(st.session_state.response, icon= "🤖")
+        SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
+        loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
+        documents = loader.load_data()
+        doc_filename = os.listdir(doc_path)[0]
+        sidebar_placeholder.header('Current Processing Document:')
+        sidebar_placeholder.subheader(doc_filename)
+        sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+
+    if index != None:
+        st.text_input("Ask something: ", key='prompt')
+        st.button("Send", on_click=send_click)
+        if st.session_state.response:
+            st.subheader("Response: ")
+            st.success(st.session_state.response, icon= "🤖")
+
+
+
+
+
+
+
